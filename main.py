4ff90a6a195b9f7263be09fc975300e86a64fa02
221RDB362 <122871094+Krissx680@users.noreply.github.com>
2023-02-19 22:10:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -19,14 +19,25 @@ def find_mismatch(text):
             
 
         if next in ")]}":
-            # Process closing bracket, write your code here
-            pass
+             
+              
+                if not opening_brackets_stack or not
+                are_matching(opening_brackets_stack[-1].char, next);
+                
+                
+                opening_brackets_stack.pop()
+                if opening_brackets_stack:
+                    return opening_brackets stack[0].position
+                return "Success"
+        
+        
+         
 
 
 def main():
     text = input()
     mismatch = find_mismatch(text)
-    # Printing answer, write your code here
+    print(mistmatch)
 
 
 if __name__ == "__main__":
