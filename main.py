5107a9a67529cb305f926a94e6125677343cec56
221RDB362 <122871094+Krissx680@users.noreply.github.com>
2023-02-19 22:40:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,27 +9,25 @@ def are_matching(left, right):
 
 
 def find_mismatch(text):
-  opening_brackets_stack = []
-  for i, next in enumerate(text):
-    if next in "([{":
-      opening_brackets_stack.append(Bracket(next, i + 1))
+    opening_brackets_stack = []
+    for i, next in enumerate(text):
+        if next in "([{":
+            opening_brackets_stack.append(Bracket(next, i+1))
 
-    if next in ")]}":
-      if not opening_brackets_stack or not are_matching(
-          opening_brackets_stack[-1].char, next):
-        return i + 1
-      opening_brackets_stack.pop()
+        if next in ")]}":
+            if not opening_brackets_stack or not are_matching(opening_brackets_stack[-1].char, next):
+                return i+1
+            opening_brackets_stack.pop()
 
-  if opening_brackets_stack:
+    if not opening_brackets_stack:
+        return "Success"
+
     return opening_brackets_stack[0].position
-
-  return "Success"
 
 
 def main():
   text = input()
   mismatch = find_mismatch(text)
-  
   print(mismatch)
 
 
